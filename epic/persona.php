4f0c3d3cf2b8db7bd1feace86df7c6c2c89c1927
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-13 10:11:57 -0600

    Created use case for Johnny in data design project

--- epic/persona.php
+++ epic/persona.php
@@ -10,16 +10,16 @@
 		</h1>
 		<main>
 			<h2>Johnny McDucketts/21 years old</h2>
-			<h3>Occupation:</h3>
+			<h3>Occupation</h3>
 			<p>Student/poker prodigy at a well known midwestern university. He's a math genius, and has a reputation on campus of being an absolute grinder at the poker table, playing both online and at exclusive invitation house games. He's already making more money than most well established professionals.</p>
-			<h3>Technology:</h3>
+			<h3>Technology</h3>
 			<p>Windows user with multiple computers so he can play at multiple online poker tables simultaneously. He is a well known influencer on social media and uses his Samsung Galaxy 7 to post reviews and advertise products that he uses.</p>
-			<h3>Attitudes and Behaviors:</h3>
+			<h3>Attitudes and Behaviors</h3>
 			<p>Johnny is in class 4 hours per day, and when he's not in class he's either studying or playing poker, or doing both at the same time (like I said, he's a prodigy). He doesn't always post on social media, but when he does he has an eager audience that supports and mimics his preferences and behaviors. In addition to the money he makes at the poker table, he also is an affiliate marketer for certain poker products, including branded hats, shirts, and poker chips.</p>
 			<h3>Frustrations and Needs</h3>
 			<p>Johnny has used various e-commerce sites in the past that are not optimized for the mobile user experience. This is due to the fact that when he favorites a product that he uses he needs to do so on his PC if he wants to be able to simultaneously share his favorites with his followers on his social media platforms. He's clearly a guy who adheres to the phrase: time is money, and not being able to do all of this on his Samsung Galaxy 7 is really getting on his nerves.</p>
 			<h3>Goals</h3>
-			<p>Johnny's goal is simple. He wants to be able to share products to his social media followers thathe has favorited on an e-commerce platform, and he wants to be able to do this exclusively from his mobile device. This allows him to multi-task, while maximizing his money making potential as a poker player and affiliate marketer.</p>
+			<p>Johnny's goal is simple. He wants to be able to share products to his social media followers that he has favorited on an e-commerce platform, and he wants to be able to do this exclusively from his mobile device. This allows him to multi-task, while maximizing his money making potential as a poker player and affiliate marketer.</p>
 
 		</main>
 	</body>
---
+++ epic/use-case.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>USE CASE</title>
+	</head>
+	<body>
+		<header>
+			<h1>USE CASE</h1>
+		</header>
+
+		<main>
+			<p>Johnny McDucketts just got back from class and will be starting a highly anticipated online poker tournament shortly. He is wearing a new hat from one of his sponsors and has strategically held off on favoriting the hat and sharing it with his followers until this moment, because he knows his audience will be very active right before the tournament starts. His PC is already set up for the tournament, and he needs to login to the site on his mobile device and favorite the hat he recently received and is currently wearing, and be able to share this favorite with his audience. Timing is critical and getting it just right could mean a large difference in his affiliate marketing commission.</p>
+		</main>
+	</body>
+
+</html>
