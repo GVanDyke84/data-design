143f4971b7069f699b1b09e9e9235be29d122fe1
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-13 11:18:30 -0600

    updated conceptual model for data-design project

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,9 +10,10 @@
 
 			<main>
 				<ul>
+					<li>Profile ID is entered to login to site</li>
+					<li>Password is hashed and verified</li>
 					<li>Many products can be favorited by many profiles</li>
 					<li>Many favorites can be shared to many social network sites</li>
-
 				</ul>
 			</main>
 		</header>
