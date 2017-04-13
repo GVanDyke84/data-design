5ad836d7cd9a17696e75e85ac02fca0f18cbd393
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-13 10:33:39 -0600

    Updated persona for Johnny in data design project

--- epic/persona.php
+++ epic/persona.php
@@ -15,7 +15,7 @@
 			<h3>Technology</h3>
 			<p>Windows user with multiple computers so he can play at multiple online poker tables simultaneously. He is a well known influencer on social media and uses his Samsung Galaxy 7 to post reviews and advertise products that he uses.</p>
 			<h3>Attitudes and Behaviors</h3>
-			<p>Johnny is in class 4 hours per day, and when he's not in class he's either studying or playing poker, or doing both at the same time (like I said, he's a prodigy). He doesn't always post on social media, but when he does he has an eager audience that supports and mimics his preferences and behaviors. In addition to the money he makes at the poker table, he also is an affiliate marketer for certain poker products, including branded hats, shirts, and poker chips.</p>
+			<p>Johnny is in class 4 hours per day, and when he's not in class he's either studying or playing poker, or doing both at the same time (like I said, he's a prodigy). He is very strategic about the timing of his social media posts, usually coinciding with well known poker events. In addition to the money he makes at the poker table, he also is an affiliate marketer for certain poker products, including branded hats, shirts, and poker chips.</p>
 			<h3>Frustrations and Needs</h3>
 			<p>Johnny has used various e-commerce sites in the past that are not optimized for the mobile user experience. This is due to the fact that when he favorites a product that he uses he needs to do so on his PC if he wants to be able to simultaneously share his favorites with his followers on his social media platforms. He's clearly a guy who adheres to the phrase: time is money, and not being able to do all of this on his Samsung Galaxy 7 is really getting on his nerves.</p>
 			<h3>Goals</h3>
