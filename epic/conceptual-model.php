0e4a146c6cc0fb6e36ba79a4eefa373b9d737374
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-13 11:08:28 -0600

    Created conceptual model for data-design project

---
+++ epic/conceptual-model.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>CONCEPTUAL MODEL</title>
+	</head>
+	<body>
+		<header>
+			<h1>CONCEPTUAL MODEL</h1>
+
+			<main>
+				<ul>
+					<li>Many products can be favorited by many profiles</li>
+					<li>Many favorites can be shared to many social network sites</li>
+
+				</ul>
+			</main>
+		</header>
+	</body>
+</html>
