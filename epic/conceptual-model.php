a44cda3a9268dbb97dfc127c25deea716c174ab8
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-13 11:48:39 -0600

    updated conceptual model for data-design project

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -14,6 +14,10 @@
 					<li>Password is hashed and verified</li>
 					<li>Many products can be favorited by many profiles</li>
 					<li>Many favorites can be shared to many social network sites</li>
+					<li>Profile entity has identifying attributes such as avatar, physical location email address, social media profiles</li>
+					<li>Product entity has identifying attributes such as photo, price, size/dimensions</li>
+					<li>Favorite entity is a heart icon that when clicked on initiates a prompt allowing one to share their favorite to the social networks of choice</li>
+
 				</ul>
 			</main>
 		</header>
