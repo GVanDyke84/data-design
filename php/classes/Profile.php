18dba02bcc288788bd79a853450c9eb17ecbe523
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-19 13:23:29 -0600

    updated based on feedback from @deepdivedylan :heart_eyes_cat:

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,34 +1,72 @@
 <?php
-namespace\;
+//namespace\;
 require_once("autoload.php");
-/**This is an example of a profile on an ecommerce site
+/**
+ * This is an example of a profile on an ecommerce site
+ *
+ * ...and a description here
+ *
  * @author Gerrit Van Dyke <gvandyke1@cnm.edu>
  * @version 1.0
  **/
 class Profile {
 	/**
 	 * id for the profile: this is the primary key
-	 * @var int $profileID
+	 * @var int $profileId
 	 **/
-private $profileID;
-/**
- * required email attached to the profile
- * @var string SprofileEmail
- */
-private $profileEmail;
-/**
- * optional avatar name or photo connected with profile
- * @var string $profileAvatar
- **/
-private $profileAvatar;
-/**
- * randomly generated required salt for profile password
- * @string bin2hex(random_bytes(16)) $profileSalt
- **/
-private $profileSalt;
-/**
- * randomly generated required hash for profile password
- * @string hash_pbkdf2("sha512", $password, $profileSalt, 262144);
-**/
-private $profileHash;
+	private $profileId;
+	/**
+	 * required email attached to the profile
+	 * @var string SprofileEmail
+	 */
+	private $profileEmail;
+	/**
+	 * optional avatar name or photo connected with profile
+	 * @var string $profileAvatar
+	 **/
+	private $profileAvatar;
+	/**
+	 * randomly generated required salt for profile password
+	 * @var string $profileSalt
+	 **/
+	private $profileSalt;
+	/**
+	 * randomly generated required hash for profile password
+	 * @var string $profileHash
+	 **/
+	private $profileHash;
+
+
+	/**
+	 * accessor method for profileID
+	 * @return int|null value of profileID
+	 **/
+	public function getProfileId(): ?int {
+		return ($this->profileId);
+	}
+
+	/**
+	 * mutator method for profileID
+	 * @param int|null $profileId value of profileID
+	 * @throws \RangeException if $profileID is not positive
+	 * @throws \TypeError if $profileID is not an integer
+	 * @throws \
+	 **/
+	public function setProfileId(?int $profileId): void {
+		//if profile id is null immediately return it
+		if($profileId === null) {
+			$this->profileId = null;
+			return;
+		}
+		// verify profileID is positive
+		if($profileId <= 0) {
+			throw(new \RangeException("profile id is not positive"));
+			//convert and store proileID
+			$this->profileId;
+		}
+	}
+	/**
+	 *accessor method for profile email
+	 * @return string value of profile email
+	 **/
 }
